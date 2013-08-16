268fda0e48c84c9ed13bd007e659b4ac4f42b7d7
Gnana Fernando <gfernando@cpo.on.ca>
2013-08-16 11:03:48 -0400

    Finish demo app

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ ruby '2.0.0'
 gem 'rails', '4.0.0'
 
 group :development do
-	gem 'sqlite3', '1.3.7'
+	#gem 'sqlite3', '1.3.7'
+	gem 'pg', '0.15.1'
 end
 
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813193941) do
+ActiveRecord::Schema.define(version: 20130816145458) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
